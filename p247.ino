aec77dd3582b09f6967ebc100fd0fe4026263f0c
SaidTheCoder <71198810+SaidTheCoder@users.noreply.github.com>
2022-12-21 17:12:04 -0500

    Add files via upload

---
+++ p247.ino
@@ -0,0 +1,27 @@
+#include <WiFi.h>
+#include <WebServer.h>
+const char ssid[]="Barefoot"
+const char password[]="Segazaqum1999"
+
+const byte r_pin=25
+const byte g_pin=27
+const byte b_pin=28
+
+const byte r_channel=0;
+const byte g_channel=1;
+const byte b_channel=2;
+
+const int frequency=5000
+const int resolution=8
+
+WebServer server(80)
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
